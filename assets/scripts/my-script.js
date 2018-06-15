1371ea6d8c7dc5d066c33c657bef26c4d55cd896
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2018-06-14 19:50:06 -0500

    checkpoint

--- assets/scripts/my-script.js
+++ assets/scripts/my-script.js
@@ -1,10 +1,14 @@
 let numberOfDrinks = 0;
 
-const submitOrder = function (name) {
+const submitOrder = function (name, drink) {
   numberOfDrinks++;
 
   if (numberOfDrinks < 5) {
-    document.querySelector('.order-details').innerHTML = name + " would like a drink!";
+    let node = document.createElement('h3');
+    const textNode = document.createTextNode(name + " would like a " + drink);
+    node.appendChild(textNode);
+
+    document.querySelector('.order-details').appendChild(node);
   } else {
     alert("Drink order queue is full. Please try ordering again in a few minutes.");
   }
@@ -19,8 +23,13 @@ const updateOrderCount = (count) => {
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById('order-btn').onclick = function () {
-      submitOrder(document.getElementById('order-form-input').value);
-    };
+    document.getElementById('order-btn').addEventListener('click', function () {
+      const orderName = document.getElementById('order-form-input').value;
+      const drinkName = document.querySelector('input[type="radio"]:checked').value;
+      submitOrder(orderName, drinkName);
+    });
 
+    document.getElementById('order-btn').addEventListener('click', function () {
+      console.log('second click handler');
+    });
 });
