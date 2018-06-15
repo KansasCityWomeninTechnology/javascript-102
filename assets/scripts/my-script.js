eddc55840f9c9a9cf2841153de0689a7c301aae8
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2018-06-14 19:36:56 -0500

    checkpoint

--- assets/scripts/my-script.js
+++ assets/scripts/my-script.js
@@ -1,6 +1,26 @@
+let numberOfDrinks = 0;
+
+const submitOrder = function (name) {
+  numberOfDrinks++;
+
+  if (numberOfDrinks < 5) {
+    document.querySelector('.order-details').innerHTML = name + " would like a drink!";
+  } else {
+    alert("Drink order queue is full. Please try ordering again in a few minutes.");
+  }
+
+  updateOrderCount(numberOfDrinks);
+};
+
+const updateOrderCount = (count) => {
+  document.getElementById('drink-count').innerHTML = "Drinks Ordered: " + count;
+};
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById('order-btn').onclick = function () {};
+    document.getElementById('order-btn').onclick = function () {
+      submitOrder(document.getElementById('order-form-input').value);
+    };
 
 });
