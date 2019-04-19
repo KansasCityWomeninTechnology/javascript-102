23d1b66c1e9a9451cf49e91c8f5e558f3053270a
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2019-04-18 19:20:45 -0500

    2019 updates

--- assets/scripts/my-script.js
+++ assets/scripts/my-script.js
@@ -1,26 +1,13 @@
-let numberOfDrinks = 0;
-
 const submitOrder = function (name) {
-  numberOfDrinks++;
-
-  if (numberOfDrinks < 5) {
-    document.querySelector('.order-details').innerHTML = name + " would like a drink!";
-  } else {
-    alert("Drink order queue is full. Please try ordering again in a few minutes.");
-  }
-
-  updateOrderCount(numberOfDrinks);
+    alert(name + " would like a drink!");
 };
-
-const updateOrderCount = (count) => {
-  document.getElementById('drink-count').innerHTML = "Drinks Ordered: " + count;
-};
-
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById('order-btn').onclick = function () {
-      submitOrder(document.getElementById('order-form-input').value);
+        const drinkName = document.querySelector("input[type='radio']:checked").value;
+        console.log(drinkName);
+        submitOrder(document.getElementById('order-form-input').value);
     };
 
 });
