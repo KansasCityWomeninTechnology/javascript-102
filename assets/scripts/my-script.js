a5e8c75bb3ad2b21f8d0f14b58b57c8d86b9f981
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2019-04-19 19:54:30 -0500

    2019 change to arrow functions

--- assets/scripts/my-script.js
+++ assets/scripts/my-script.js
@@ -1,13 +1,13 @@
-const submitOrder = function (name, drink) {
+const submitOrder = (name, drink) => {
     let node = document.createElement('h3');
     const textNode = document.createTextNode(name + " would like a " + drink);
     node.appendChild(textNode);
     document.querySelector('.order-details').appendChild(node);
 };
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", (event) => {
 
-    document.getElementById('order-btn').addEventListener('click', function () {
+    document.getElementById('order-btn').addEventListener('click', () => {
         const drinkName = document.querySelector('input[type="radio"]:checked').value;
         const orderName = document.getElementById('order-form-input').value;
         submitOrder(orderName, drinkName);
